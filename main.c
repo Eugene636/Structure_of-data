c34adede177c1bb14411d4dccc0341a173a95181
hugo <brigan88@yandex.ru>
2021-05-18 20:02:45 +0500

    Task 1-2

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+char* SimpleIs (int d) {
+   int c = 2;
+   while ((d % c) != 0) c++;
+   if (c == d) return "Is Simple";
+   else return "Is not Simple";
+   
+}
+int main (void) {
+  int x;
+do {
+  printf ("Enter positive integer number:\n");
+} while ((scanf("%d", &x) != 1) || (x <= 1));
+  printf ("%s", SimpleIs (x));
+  return 0;
+}
